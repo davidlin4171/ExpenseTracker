dd80d463a163a3d74824e078efb2a02652a24652
danielcao888 <dscao2@illinois.edu>
2023-10-19 23:43:12 -0500

    Added Initial Setup of Spending History Table

--- react_framework/src/index.js
+++ react_framework/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { PrimeReactProvider } from 'primereact/api';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';   // theme
+import 'primeflex/primeflex.css';                                   // css utility
+import 'primeicons/primeicons.css';
+import 'primereact/resources/primereact.css';                       // core css
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './authentication/Login';
--- react_framework/src/views/Home.js
+++ react_framework/src/views/Home.js
@@ -2,16 +2,32 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+        
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Home = () => {
 
+  //array with past spending history 
+ const pastSpending = [{
+  id: '1000',
+  code: 'f230fh0g3',
+  name: 'Rent',
+  description: 'Product Description',
+  image: 'bamboo-watch.jpg',
+  price: 65,
+  category: 'Housing',
+  quantity: 1,
+  inventoryStatus: 'INSTOCK',
+  rating: 5
+}];
  const data = {
-  labels: ['Jan', 'Feb', 'March', 'April', 'May', 'Orange'],
+  labels: ['Home', 'Shopping', 'Food/Drink', 'Entertainment/Miscellaneous', 'Transportation', 'Savings'],
   datasets: [
     {
-      label: '# of Votes',
+      label: 'Percent of Budget Spent',
       data: [12, 19, 3, 5, 2, 3],
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
@@ -22,7 +38,6 @@ const Home = () => {
         'rgba(255, 99, 132, 1)',
         'rgba(54, 162, 235, 1)',
         'rgba(255, 206, 86, 1)'
-        
       ],
       borderWidth: 1,
     },
@@ -36,7 +51,13 @@ const Home = () => {
 				<div className="chart-container">
 
 				<Doughnut data={data} />
-				</div>
+				</div> 
+        <DataTable value={pastSpending} tableStyle={{ minWidth: '50rem' }}>
+        <Column field="name" header="Name of Purchase"></Column>
+        <Column field="category" header="Category"></Column>
+        <Column field="quantity" header="Quantity"></Column>
+        <Column field="price" header="Price($)"></Column>
+        </DataTable>
 			</Container>
 		</React.Fragment>
 	)
